e07923a16e02ccc09ba44cdde5e105c6d5fe12bc
rigas <sassalosrigas@gmail.com>
2023-05-19 18:17:25 +0300

    made main menu

--- mainApp.java
+++ mainApp.java
@@ -8,7 +8,15 @@ Agamemnonas Spyrou ()
 
  public class mainApp {
     public static void main(String[] args) {
-    
+    System.out.println("1. Add new advertisment carrier");
+    System.out.println("2. Add new advertisment type");
+    System.out.println("3. Add new advertisment");
+    System.out.println("4. Show advertisments");
+    System.out.println("5. Show specific carrier advertisments");
+    System.out.println("6. Calculate specific carrier advertisment cost");
+    System.out.println("7. Show number of advertisments per product");
+    System.out.println("8. Calculate specific product advertisment cost");
+    System.out.println("9. Show advertisment cost per product");
     }
  }
  
