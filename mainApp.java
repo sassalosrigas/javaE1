d648d1604e27043995cb35fbd13edcfb2afb53bb
rigas <sassalosrigas@gmail.com>
2023-05-19 18:41:00 +0300

    started switch in main

--- mainApp.java
+++ mainApp.java
@@ -5,18 +5,46 @@ Rigas Sassalos (3220178)
 Anais Farchat (3220280)
 Agamemnonas Spyrou ()
  */
+import java.util.Scanner;
 
  public class mainApp {
     public static void main(String[] args) {
-    System.out.println("1. Add new advertisment carrier");
-    System.out.println("2. Add new advertisment type");
-    System.out.println("3. Add new advertisment");
-    System.out.println("4. Show advertisments");
-    System.out.println("5. Show specific carrier advertisments");
-    System.out.println("6. Calculate specific carrier advertisment cost");
-    System.out.println("7. Show number of advertisments per product");
-    System.out.println("8. Calculate specific product advertisment cost");
-    System.out.println("9. Show advertisment cost per product");
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Choose action:")
+        System.out.println("1. Add new advertisment carrier");
+        System.out.println("2. Add new advertisment type");
+        System.out.println("3. Add new advertisment");
+        System.out.println("4. Show advertisments");
+        System.out.println("5. Show specific carrier advertisments");
+        System.out.println("6. Calculate specific carrier advertisment cost");
+        System.out.println("7. Show number of advertisments per product");
+        System.out.println("8. Calculate specific product advertisment cost");
+        System.out.println("9. Show advertisment cost per product");
+
+        int res = input.nextInt();
+
+        switch(res) {
+            case 1:
+
+            case 2:
+
+            case 3:
+
+            case 4:
+
+            case 5:
+
+            case 6:
+
+            case 7:
+
+            case 8:
+
+            case 9:
+            
+        }
+
     }
  }
  
